0600a15d1a79e77e1d833c1be11b111b4031bb2e
CapitanMikon <77883550+CapitanMikon@users.noreply.github.com>
2021-12-11 18:08:37 +0100

    fixed folder name

--- experiments/maybe_useful_tools/generate_normalize_vid_batch_file.bat
+++ experiments/maybe_useful_tools/generate_normalize_vid_batch_file.bat
@@ -4,7 +4,7 @@ SET generated_file_name=normalize_videos_batch2
 echo Starting generating process of making batch for converting to 30 fps videos....
 echo @echo off >> %generated_file_name%.bat
 echo echo Starting process converting fps to 30..... >> %generated_file_name%.bat
-echo mkdir samples_raw_30fps >> %generated_file_name%.bat
+echo mkdir specimens >> %generated_file_name%.bat
 for /f %%f in ('dir /b samples_raw_cut') do echo ffmpeg -i "samples_raw_cut\%%f" -max_muxing_queue_size 4096 -r 30 -y "samples_raw_30fps\%%f" >> %generated_file_name%.bat
 echo echo Ended >> %generated_file_name%.bat
 echo pause >> %generated_file_name%.bat
